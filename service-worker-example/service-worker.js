ff09ed2727f78b5227a5c34c4d9a55ef4d483f7d
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-05-12 14:15:55 +0200

    Added code

--- service-worker-example/service-worker.js
+++ service-worker-example/service-worker.js
@@ -1,4 +1,9 @@
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll(['index.html', 'js/index.js', 'offline.html', 'favicon.ico', 'css/style-offline.css']);
+        })
+    );
     self.skipWaiting();
     console.log('Service worker installed!');
 });
@@ -14,8 +19,13 @@ self.addEventListener('fetch', (event) => {
         console.log('Du är online!');
     } else {
         console.log('Du är offline!');
-        event.respondWith(new Response('<h1>I find your lack of internet disturbing</h1>', {
-            'headers': { 'Content-Type': 'text/html'}
-        }));
+        console.log('Begärd url:', event.request);
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                console.log('response', response);
+                if (response) return response;
+                else return caches.match(new Request('offline.html'));
+            })
+        );
     }
 });
