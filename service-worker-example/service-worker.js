007635eafd244d8063b5e2256da2e4fc74196c1b
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-05-12 11:25:17 +0200

    Added code

--- service-worker-example/service-worker.js
+++ service-worker-example/service-worker.js
@@ -10,4 +10,12 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     console.log(event.request.url); // Skriv ut url:en på varje nätverksförfrågan
+    if (navigator.onLine) {
+        console.log('Du är online!');
+    } else {
+        console.log('Du är offline!');
+        event.respondWith(new Response('<h1>I find your lack of internet disturbing</h1>', {
+            'headers': { 'Content-Type': 'text/html'}
+        }));
+    }
 });
